bc6816785e2c9a38af7f2ce655e12d2791e4677a
idir-44 <idirmoudache+github@gmail.com>
2024-01-15 14:53:00 +0100

    hash-set

---
+++ hashtables/hashset.go
@@ -0,0 +1,23 @@
+package hashtables
+
+// dumb response for leetcode
+// use maps instead
+type MyHashSet struct {
+	set [1000001]bool
+}
+
+func Constructor() MyHashSet {
+	return MyHashSet{}
+}
+
+func (this *MyHashSet) Add(key int) {
+	this.set[key] = true
+}
+
+func (this *MyHashSet) Remove(key int) {
+	this.set[key] = false
+}
+
+func (this *MyHashSet) Contains(key int) bool {
+	return this.set[key]
+}
